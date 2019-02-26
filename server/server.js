c8fb08b7a56132a9f3546504b5bfac9c5851e925
udgin80 <udgin.80@mail.ru>
2019-02-26 10:18:14 +0300

    Add geoLocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -1,14 +1,46 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app') );
+
+   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Has been joined us'));
+
+    socket.on('createMessage', (message, callback) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server');
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', 
+        generateLocationMessage('Admin', coords.latitude,coords.longitude));
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected')
+    });
+});
+
+//https://w.google.com/maps/
+
+
+server.listen(port, () => {
     console.log('Server is pu on port 3000');
 });
